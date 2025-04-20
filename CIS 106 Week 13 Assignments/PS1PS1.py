cc0c2d57d4ed3866682075a99f0d87adcc5f603d
sfernandez2773 <s_fernandez7@mail.harpercollege.edu>
2025-04-20 15:19:19 -0500

    Add files via upload

---
+++ CIS 106 Week 13 Assignments/PS1PS1.py
@@ -0,0 +1,55 @@
+
+List1=[]
+List1= (List1)
+print(List1) 
+print(List1)
+
+#Number 2
+List1.insert(0,99)
+print(List1)
+
+#Number 3
+List1[0]= 100
+print(List1)
+#Number 4
+List2= ["500","600","700","800","900"]
+List1.extend(List2)
+print(List1)
+#Number 5
+List2.remove("800")
+print(List2)
+#Number 6
+List2.remove("700")
+print(List2)
+#Number 7
+List3= ["A","B","C","A","A","C"]
+#Number 8
+print(List3.index("A"))
+#Number 9
+print(List3.count("B"))
+#Number 10
+if "F" in List3:
+    List3.remove("F")
+else:
+    print("F is not included")  # No grade of "F" from List3
+#Number 11
+List2.clear()
+print(List2)
+#Number 13 and 14
+List1.append(["Rizzo", "Davis", "Baez", "Happ", "Bryan"])
+print(List1)
+#Number 15
+List2= ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+List2.sort()
+print(List2)
+#Number 16
+List2.reverse()
+print(List2) 
+
+
+
+
+
+
+    
+
